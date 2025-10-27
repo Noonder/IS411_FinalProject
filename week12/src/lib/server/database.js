acf0ca1a310772a4d9af2d06a95547fb023f9562
Thitaporn Chuchan <thitaporn.chuc@dome.tu.ac.th>
2025-10-27 16:39:19 +0700

    20251027

---
+++ week12/src/lib/server/database.js
@@ -0,0 +1,37 @@
+const db = new Map()
+
+// @ts-ignore
+export function getTodos(userid) {
+    if (!db.get(userid)) {
+        db.set(userid, [{
+            id: crypto.randomUUID(),
+            description: 'Learn Svelte Kit',
+            done: false
+        }])
+
+    }
+
+    return db.get(userid)
+}
+
+// @ts-ignore
+export function createTodos(userid,description) {
+    const todos = db.get(userid)
+
+    todos.push({
+        id: crypto.randomUUID(),
+        description,
+        done: false
+    })
+}
+
+// @ts-ignore
+export function deleteTodos(userid,tpdpid) {
+    const todos = db.get(userid)
+    // @ts-ignore
+    const index = todos.findIndex((todo) => todo.id === todoid)
+
+    if(index !== -1){
+        todos.splice(index,1)
+    }
+}
